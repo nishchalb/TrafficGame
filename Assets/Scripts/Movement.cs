a166100d3107ee36306f7e127e81a21ce01a3ac7
Nishchal Bhandari <nishchal@mit.edu>
2017-04-29 17:40:24 -0400

    Car follow logic good, turning bad

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,6 +8,7 @@ public class Movement : MonoBehaviour {
     public float turnRate = 1.0f;
     public float maxVelocity = 10f;
     public float acceleration = 1.0f;
+    public float maxDist;    public float decel;
 
     private Rigidbody2D rb;
     private Vector3 targetDir;
@@ -21,24 +22,25 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        targetDir = nextWaypoint.transform.position - transform.position;
-        angDiff = Vector2.Angle(transform.up, targetDir);
+        
     }
 
     private void FixedUpdate()
     {
-        
-        
+        targetDir = nextWaypoint.transform.position - transform.position;
+        angDiff = Vector2.Angle(transform.up, targetDir);
+
         //if (Mathf.Abs(angDiff) > .1)
         if (Vector2.Dot(transform.up, targetDir) < .99)
         {
-            Debug.Log(Vector2.Dot(transform.up, targetDir));
+            
             rb.velocity = Vector2.zero;
             Vector3 cross = Vector3.Cross(transform.up, targetDir);
             if (cross.z < 0) angDiff *= -1;
+            Debug.Log(angDiff);
             rb.rotation += turnRate * angDiff;
             transform.up = Vector2.Lerp(transform.up, targetDir, .1f);
-            // rb.AddForce(-1 * rb.velocity);
+            // rb.AddForce(-1 * rb.velocity); disabled for now in favor of immediate stopping
         } else
         {
             // Move towards waypoint
@@ -47,7 +49,19 @@ public class Movement : MonoBehaviour {
                 rb.AddForce(transform.up*acceleration);
             }
         }
-        
+
+        // Dont run into other cars
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);        float distance = Vector2.Distance(transform.position, hit.point);        if (hit.collider.tag == "Car" && distance < maxDist && Vector2.Dot(rb.velocity, transform.up) > 0)        {
+            rb.AddForce(rb.velocity.magnitude * -decel * transform.up * (1 / distance));
+            // rb.velocity *= 1 - decel * (1/hit.distance);
+            if (Vector2.Dot(rb.velocity, transform.up) < 0 || distance < 1.5)
+            {
+                rb.velocity = Vector2.zero;
+            }        }
+
+        Debug.DrawRay(transform.position, targetDir);
+
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
