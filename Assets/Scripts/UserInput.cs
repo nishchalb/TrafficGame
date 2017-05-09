cb99a5caf684edd542e6f03796f7ae805dc4ff7f
RUTH <RUTHPARK@MIT.EDU>
2017-05-08 21:18:02 -0400

    saving settings for diff traffic lights almost working

--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -4,59 +4,76 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UserInput : MonoBehaviour {
-	public GameObject light;
+
 	public InputField verticalgreen;
 	public InputField horizontalgreen;
 	public InputField cycleTime;
 	public InputField offset;
 	public Toggle vfirst;
 
-	private Dictionary<string, Dictionary<string, int>> settings;
-	private string trafficLight;
+	private Dictionary<string, Dictionary<string, string>> settings;
+	private string trafficLight; // this is determined from the click
+	private Dictionary<string, InputField> mapping; 
+	// add vfirst to above at some point
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("UserInput is working");
-		settings = new Dictionary<string, Dictionary<string, int>> ();
-		
+		settings = new Dictionary<string, Dictionary<string, string>> ();
+		mapping = new Dictionary<string, InputField>()
+		{
+			{"verticalgreen", verticalgreen},
+			{"horizontalgreen", horizontalgreen},
+			{"cycleTime", cycleTime},
+			{"offset", offset},
+		};
+		verticalgreen.onValueChanged.AddListener (delegate {UpdateFields("verticalgreen"); });
+		horizontalgreen.onValueChanged.AddListener (delegate {UpdateFields("horizontalgreen"); });
+		cycleTime.onValueChanged.AddListener (delegate {UpdateFields("cycleTime"); });
+		offset.onValueChanged.AddListener (delegate {UpdateFields("offset"); });
+		// do for vfirst
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
-			Debug.Log("something was clicked!");
 			Ray ray = Camera.main.ScreenPointToRay(
 				Input.mousePosition);
 			RaycastHit hit;
 			if (Physics.Raycast(ray, out hit, 100)) {
-				trafficLight = hit.transform.gameObject.name;
-				if (settings.ContainsKey (trafficLight)) {
-					
-				} else {
+				if (trafficLight == null) {
+					trafficLight = hit.transform.gameObject.name;
+				}
+				else if (trafficLight != hit.transform.gameObject.name) {
 					ResetInputs ();
-					if (cycleTime.text.Length > 0) {
-						
+					if (settings.ContainsKey (trafficLight)) {
+						FillInputs ();
 					}
-
+					trafficLight = hit.transform.gameObject.name;
 				}
 			}
 		}
 	}
 	void ResetInputs () {
-		// reset all inputs
+		verticalgreen.text = "";
+		horizontalgreen.text = "";
+		cycleTime.text = "";
+		offset.text = "";
 	}
 
 	void FillInputs () {
 		// fill inputs with appropriate vals
+		foreach (var item in settings[trafficLight]) {
+			mapping[item.Key].text = item.Value;
+		}
+
 	}
 
-	void UpdateSettings(string objectName, string parameter, int value) {
-		// either add or update object
-		if (settings.ContainsKey (trafficLight)) {
-			settings [trafficLight] [parameter] = value;
-		} else {
-			settings [trafficLight] = new Dictionary<string, int> ();
-			UpdateSettings (objectName, parameter, value);
+	public void UpdateFields(string param) {
+		// if trafficLight null, ask which traffic light?
+		if (trafficLight != null) {
+			if (!settings.ContainsKey (trafficLight))
+			settings [trafficLight] [param] = mapping[param].text;
 		}
 	}
 }
