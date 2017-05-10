61aeb7f293b7eb8e95924115043c0725bbadda3c
RUTH <RUTHPARK@MIT.EDU>
2017-05-09 21:42:27 -0400

    debugging dictionary issues

--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -12,7 +12,7 @@ public class UserInput : MonoBehaviour {
 	public Toggle vfirst;
 
 	private Dictionary<string, Dictionary<string, string>> settings;
-	private string trafficLight; // this is determined from the click
+	public string trafficLight; // this is determined from the click
 	private Dictionary<string, InputField> mapping; 
 	// add vfirst to above at some point
 
@@ -46,10 +46,10 @@ public class UserInput : MonoBehaviour {
 				}
 				else if (trafficLight != hit.transform.gameObject.name) {
 					ResetInputs ();
+					trafficLight = hit.transform.gameObject.name;
 					if (settings.ContainsKey (trafficLight)) {
 						FillInputs ();
 					}
-					trafficLight = hit.transform.gameObject.name;
 				}
 			}
 		}
@@ -63,8 +63,10 @@ public class UserInput : MonoBehaviour {
 
 	void FillInputs () {
 		// fill inputs with appropriate vals
-		foreach (var item in settings[trafficLight]) {
-			mapping[item.Key].text = item.Value;
+		foreach (string key in settings[trafficLight].Keys) {
+			Debug.Log ("item is " + key + " " + settings[trafficLight][key]);
+//			Debug.Log ("dic is " + settings [trafficLight].ToString());
+			mapping[key].text = settings[trafficLight][key];
 		}
 
 	}
@@ -72,8 +74,11 @@ public class UserInput : MonoBehaviour {
 	public void UpdateFields(string param) {
 		// if trafficLight null, ask which traffic light?
 		if (trafficLight != null) {
-			if (!settings.ContainsKey (trafficLight))
+			if (!settings.ContainsKey (trafficLight)) {
+				settings [trafficLight] = new Dictionary<string, string> ();
+			}
 			settings [trafficLight] [param] = mapping[param].text;
+			Debug.Log ("inserting into dic " + param + " " + settings[trafficLight][param]);
 		}
 	}
 }
